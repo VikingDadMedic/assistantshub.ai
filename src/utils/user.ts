d258f55f4903c615d906a34ef4bdf75708e0b069
Ryan E <91684138+VikingDadMedic@users.noreply.github.com>
2024-10-24 13:20:28 -0400

    Update user.ts

--- src/utils/user.ts
+++ src/utils/user.ts
@@ -1,10 +1,14 @@
+// Function to check if a user is an admin
 export const isAdminUser = function (user: any) {
+  // Check if the environment variable ADMIN_USERS is set
   if (process.env.ADMIN_USERS) {
-    // const allowedAdmins = JSON.parse(process.env.ADMIN_USERS);
-    const allowedAdmins = 'voyagersocialio@gmail.com';
+    // Parse the ADMIN_USERS environment variable to get the list of allowed admin user IDs
+    const allowedAdmins = JSON.parse(process.env.ADMIN_USERS);
 
+    // Return true if the user exists and their ID is in the list of allowed admin user IDs
     return user && allowedAdmins.includes(user.sub);
   } else {
+    // Return false if the ADMIN_USERS environment variable is not set
     return false;
   }
 };
