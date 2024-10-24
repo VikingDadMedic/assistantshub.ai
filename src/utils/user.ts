ac5c4c2b695c7781876081cc251bf160b6a4874b
Ryan E <91684138+VikingDadMedic@users.noreply.github.com>
2024-10-24 03:34:17 -0400

    Update user.ts

--- src/utils/user.ts
+++ src/utils/user.ts
@@ -1,6 +1,6 @@
 export const isAdminUser = function (user: any) {
   if (process.env.ADMIN_USERS) {
-    const allowedAdmins = JSON.parse(process.env.ADMIN_USERS);
+    const allowedAdmins = 'voyagersocialio@gmail.com';
 
     return user && allowedAdmins.includes(user.sub);
   } else {
